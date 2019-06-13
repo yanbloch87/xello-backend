062bcdfe1bbd350e21e823081be8636c67aa4bdc
Yan Bloch <yan.bloch@startapp.com>
2019-06-13 11:45:24 +0300

    first commit

---
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -0,0 +1,226 @@
+﻿namespace GraduationTracker.Tests.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines the <see cref="GraduationTrackerTests" />
+    /// </summary>
+    [TestClass]
+    public class GraduationTrackerTests
+    {
+        private GraduationTracker tracker = new GraduationTracker();
+        private Diploma diploma = new Diploma()
+        {
+            Id = 1,
+            Credits = 4,
+            Requirements = new int[] { 100, 102, 103, 104 }
+        };
+
+        /// <summary>
+        /// The TestHasCredits
+        /// </summary>
+        [TestMethod]
+        public void TestHasCredits()
+        {
+
+            var students = new[]
+            {
+               new Student
+               {
+                   Id = 1,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=95 },
+                        new Course{Id = 2, Name = "Science", Mark=95 },
+                        new Course{Id = 3, Name = "Literature", Mark=95 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
+                   }
+               },
+               new Student
+               {
+                   Id = 2,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=80 },
+                        new Course{Id = 2, Name = "Science", Mark=80 },
+                        new Course{Id = 3, Name = "Literature", Mark=80 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
+                   }
+               },
+                new Student
+                {
+                    Id = 3,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=50 },
+                        new Course{Id = 2, Name = "Science", Mark=50 },
+                        new Course{Id = 3, Name = "Literature", Mark=50 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=50 }
+                    }
+                },
+                new Student
+                {
+                    Id = 4,
+                    Courses = new Course[]
+                    {
+                        new Course{Id = 1, Name = "Math", Mark=40 },
+                        new Course{Id = 2, Name = "Science", Mark=40 },
+                        new Course{Id = 3, Name = "Literature", Mark=40 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                    }
+                }
+            };
+
+            var graduated = new List<Tuple<bool, STANDING>>();
+
+            foreach (var student in students)
+            {
+                var hasRemedial = tracker.HasGraduated(diploma, student);
+                if (hasRemedial.Item1 == true)
+                {
+                    graduated.Add(hasRemedial);
+                }
+            }
+
+            Assert.IsFalse(graduated.Count == students.Length);
+        }
+
+        /// <summary>
+        /// The TestHasCredits
+        /// </summary>
+        [TestMethod]
+        public void TestHasRemedial()
+        {
+            var student = new Student
+            {
+                Id = 1,
+                Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=49 },
+                        new Course{Id = 2, Name = "Science", Mark=49 },
+                        new Course{Id = 3, Name = "Literature", Mark=49 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=49 }
+                   }
+            };
+
+            var hasRemedial = tracker.HasGraduated(diploma, student);
+
+            Assert.IsTrue(hasRemedial.Item1 == false && hasRemedial.Item2 == STANDING.Remedial);
+        }
+
+        /// <summary>
+        /// The TestHasAverage
+        /// </summary>
+        [TestMethod]
+        public void TestHasAverage()
+        {
+            var student = new Student
+            {
+                Id = 1,
+                Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=79 },
+                        new Course{Id = 2, Name = "Science", Mark=79 },
+                        new Course{Id = 3, Name = "Literature", Mark=79 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=79 }
+                   }
+            };
+
+            var hasRemedial = tracker.HasGraduated(diploma, student);
+
+            Assert.IsTrue(hasRemedial.Item1 == true && hasRemedial.Item2 == STANDING.Average);
+        }
+
+        /// <summary>
+        /// The TestHasMagnaCumLaude
+        /// </summary>
+        [TestMethod]
+        public void TestHasMagnaCumLaude()
+        {
+            var student = new Student
+            {
+                Id = 1,
+                Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=94 },
+                        new Course{Id = 2, Name = "Science", Mark=94 },
+                        new Course{Id = 3, Name = "Literature", Mark=94 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=94 }
+                   }
+            };
+
+            var hasRemedial = tracker.HasGraduated(diploma, student);
+
+            Assert.IsTrue(hasRemedial.Item1 == true && hasRemedial.Item2 == STANDING.MagnaCumLaude);
+        }
+
+        /// <summary>
+        /// The TestHasSumaCumLaude
+        /// </summary>
+        [TestMethod]
+        public void TestHasSumaCumLaude()
+        {
+            var student = new Student
+            {
+                Id = 1,
+                Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=100 },
+                        new Course{Id = 2, Name = "Science", Mark=100 },
+                        new Course{Id = 3, Name = "Literature", Mark=100 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=100 }
+                   }
+            };
+
+            var hasRemedial = tracker.HasGraduated(diploma, student);
+
+            Assert.IsTrue(hasRemedial.Item1 == true && hasRemedial.Item2 == STANDING.SumaCumLaude);
+        }
+
+        /// <summary>
+        /// The TestHasSumaCumLaude
+        /// </summary>
+        [TestMethod]
+        public void TestHasStudent()
+        {
+            try
+            {
+                tracker.HasGraduated(diploma, null);
+            }
+            catch (NullReferenceException e)
+            {
+                Assert.IsTrue(e != null);
+            }
+        }
+
+        /// <summary>
+        /// The TestHasSumaCumLaude
+        /// </summary>
+        [TestMethod]
+        public void TestHasDiploma()
+        {
+            var student = new Student
+            {
+                Id = 1,
+                Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=100 },
+                        new Course{Id = 2, Name = "Science", Mark=100 },
+                        new Course{Id = 3, Name = "Literature", Mark=100 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=100 }
+                   }
+            };
+            try
+            {
+                tracker.HasGraduated(null, student);
+            }
+            catch (NullReferenceException e)
+            {
+                Assert.IsTrue(e != null);
+            }
+        }
+    }
+}
---
+++ GraduationTracker/GraduationTracker/BL/GraduationTracker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker
+{
+    public partial class GraduationTracker
+    {   
+        public Tuple<bool, STANDING>  HasGraduated(Diploma diploma, Student student)
+        {
+            var credits = 0;
+            var sum = 0;
+        
+            for(int i = 0; i < diploma.Requirements.Length; i++)
+            {
+                var requirement = Repository.GetRequirement(diploma.Requirements[i]);
+                for (int j = 0; j < student.Courses.Length; j++)
+                {
+                    for (int k = 0; k < requirement.Courses.Length; k++)
+                    {
+                        if (requirement.Courses[k] == student.Courses[j].Id)
+                        {
+                            sum += student.Courses[j].Mark;
+                            if (student.Courses[j].Mark > requirement.MinimumMark)
+                            {
+                                credits += requirement.Credits;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var average = sum / student.Courses.Length;
+            var hasEnoughCreadits = credits == diploma.Credits;
+
+            if (average < 50)
+                return new Tuple<bool, STANDING>(hasEnoughCreadits, STANDING.Remedial);
+            else if (average < 80)
+                return new Tuple<bool, STANDING>(hasEnoughCreadits, STANDING.Average);
+            else if (average < 95)
+                return new Tuple<bool, STANDING>(hasEnoughCreadits, STANDING.MagnaCumLaude);
+            else
+                return new Tuple<bool, STANDING>(hasEnoughCreadits, STANDING.SumaCumLaude);
+        }
+    }
+}
